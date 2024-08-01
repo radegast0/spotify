cea50b3dc4940b444aacb5a33f07a0a3762c1f63
radegast0 <fatih.yonucuoglu@gmail.com>
2024-08-01 14:13:11 +0300

    lights

--- app/experience/lights/Lights.js
+++ app/experience/lights/Lights.js
@@ -1,14 +1,9 @@
-import { Environment, SpotLight, useDepthBuffer } from "@react-three/drei";
 import React from "react";
-import { DirectionalLight } from "three";
 
 const Lights = () => {
-  const depthBuffer = useDepthBuffer();
   return (
     <>
       <ambientLight intensity={0.5} />
-      
-    
     </>
   );
 };
