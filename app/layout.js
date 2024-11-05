c0c9a67e21394b168bf90b8fe02db761af1bc34e
radegast0 <fatih.yonucuoglu@gmail.com>
2024-11-06 00:19:41 +0300

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Analytics />
         {children}
+        <Analytics />
       </body>
     </html>
   );
